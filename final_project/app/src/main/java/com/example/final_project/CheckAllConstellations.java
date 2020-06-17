5b06a3e0e6dc14967ec5c68cd7109423cac60b54
shashishajin <t107590021@ntut.org.tw>
2020-06-18 00:58:32 +0800

    fix cardview bug

--- final_project/app/src/main/java/com/example/final_project/CheckAllConstellations.java
+++ final_project/app/src/main/java/com/example/final_project/CheckAllConstellations.java
@@ -36,5 +36,49 @@ public class CheckAllConstellations extends AppCompatActivity {
         // Initialize the adapter and set it to the RecyclerView.
         mAdapter = new ConstellationAdapter(this, mSportsData);
         mRecyclerView.setAdapter(mAdapter);
+        initializeData();
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(mSportsData, from, to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                mSportsData.remove(viewHolder.getAdapterPosition());
+                mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+            }
+        });
+        helper.attachToRecyclerView(mRecyclerView);
+    }
+    private void initializeData() {
+        // Get the resources from the XML file.
+        String[] sportsList = getResources()
+                .getStringArray(R.array.sports_titles);
+        String[] sportsInfo = getResources()
+                .getStringArray(R.array.sports_info);
+        // typed array allows storing array of some XML resources
+        sportsImageResources = getResources().obtainTypedArray(R.array.sports_images);
+
+        // Clear the existing data (to avoid duplication).
+        mSportsData.clear();
+
+        // Create the ArrayList of Sports objects with titles and
+        // information about each sport.
+        for (int i = 0; i < sportsList.length; i++) {
+            mSportsData.add(new Constellations(sportsList[i], sportsInfo[i], sportsImageResources.getResourceId(i, 0)));
+        }
+        sportsImageResources.recycle();
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public void resetSports(View view) {
+        initializeData();
     }
 }
--- final_project/app/src/main/java/com/example/final_project/ConstellationDetail.java
+++ final_project/app/src/main/java/com/example/final_project/ConstellationDetail.java
@@ -1,4 +1,29 @@
 package com.example.final_project;
 
-public class ConstellationDetail {
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class ConstellationDetail extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_constellations_detail);
+
+        TextView sportsTitle = findViewById(R.id.titleDetail);
+        ImageView sportsImage = findViewById(R.id.sportsImageDetail);
+
+        Intent intent = getIntent();
+        String titleString = intent.getStringExtra("title");
+        int imageInt = intent.getIntExtra("image_resource", 0);
+
+        sportsTitle.setText(titleString);
+        Glide.with(this).load(imageInt).into(sportsImage);
+    }
 }
